a4e9c893abee08cc0e2dd397a40c0a30405baae3
Daniel Schwarz <dan@butter.sh>
2021-12-04 21:08:52 +0100

    turn out you can cast a string to bit after all ...
    and that's why you shouldn't code at 2 am ...

--- day_03_2.sql
+++ day_03_2.sql
@@ -34,19 +34,7 @@ UNION ALL
 )
 SELECT max(data_int) * min(data_int) -- there is no multiply aggregate 
 FROM (
-    SELECT -- I couldn't find a way to cast a string to a bitstring ...
-        substr(data, 12, 1)::int * 2^0 +
-        substr(data, 11, 1)::int * 2^1 +
-        substr(data, 10, 1)::int * 2^2 +
-        substr(data,  9, 1)::int * 2^3 +
-        substr(data,  8, 1)::int * 2^4 +
-        substr(data,  7, 1)::int * 2^5 +
-        substr(data,  6, 1)::int * 2^6 +
-        substr(data,  5, 1)::int * 2^7 +
-        substr(data,  4, 1)::int * 2^8 +
-        substr(data,  3, 1)::int * 2^9 +
-        substr(data,  2, 1)::int * 2^10 +
-        substr(data,  1, 1)::int * 2^11 AS data_int
+    SELECT data::bit(12)::int
     FROM foo
     WHERE (is_generator = TRUE  AND pos = ANY(SELECT max(pos) FROM foo WHERE is_generator = TRUE))
        OR (is_generator = FALSE AND pos = ANY(SELECT max(pos) FROM foo WHERE is_generator = FALSE))
